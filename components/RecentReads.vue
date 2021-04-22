e7f04f8466f283d8cc8fc8b2bc59b57c2b7c39a3
louiscollinsjr <louiscollinsjr@gmail.com>
2021-04-22 16:15:17 -0500

    Update RecentReads.vue
    Added Bookmark SVG

--- components/RecentReads.vue
+++ components/RecentReads.vue
@@ -1,6 +1,8 @@
 <template>
   <div class="p-2 text-xs">
-   Continue Reading: Genesis chapter 3
+    <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-3 float-left mr-2" viewBox="0 0 20 20" fill="currentColor">
+      <path d="M5 4a2 2 0 012-2h6a2 2 0 012 2v14l-5-2.5L5 18V4z" />
+    </svg> Continue Reading: Genesis chapter 3
   </div>
 </template>
 
